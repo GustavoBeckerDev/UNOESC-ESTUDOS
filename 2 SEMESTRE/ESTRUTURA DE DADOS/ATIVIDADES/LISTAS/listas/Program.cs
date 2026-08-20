5e848f94a9103e1c3d316f339af237fd2b630e9d
Gustavo de Abreu Becker <447876@unoesc.lan>
2026-08-19 22:06:55 -0300

    INICIO DE ATIVIDADES DE LISTAS

---
+++ 2 SEMESTRE/ESTRUTURA DE DADOS/ATIVIDADES/LISTAS/listas/Program.cs
@@ -0,0 +1,25 @@
+﻿// ArrayLists
+
+using System.Collections;
+
+ArrayList arrayList = new ArrayList();
+
+// ADICIONANDO ITENS A LISTA
+
+arrayList.Add(5);
+
+// ADICIONANDO QUANTIAS DE ITENS A LISTA
+
+arrayList.AddRange(new int[]{ 1, 2, 3 });
+
+// INSERE O VALOR 7.8 NA POSICAO 3 DA LISTA
+
+arrayList.Insert( 3, 7.8);
+
+// PERCORRENDO ITENS DA LISTA DE FORMA GENÉRICA
+
+foreach (object obj in arrayList)
+{
+    Console.WriteLine(obj);
+}
+
